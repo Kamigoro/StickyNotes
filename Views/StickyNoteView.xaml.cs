0ead9b080a1ab68d4a387ca4b9051ec2597c603b
Dylan Piette <44063839+Kamigoro@users.noreply.github.com>
2020-05-20 19:09:35 +0200

    Skeleton for adding sticky note

---
+++ Views/StickyNoteView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace StickyGO.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour StickyNoteView.xaml
+    /// </summary>
+    public partial class StickyNoteView : Window
+    {
+        public StickyNoteView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddStickyNote_Click(object sender, RoutedEventArgs e)
+        {
+            StickyNoteView view = new StickyNoteView();
+            view.Show();
+        }
+
+        private void btnSaveStickyNote_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
