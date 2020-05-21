521ac86216a26a29dff676f1492fa62b30f57f36
Dylan Piette <44063839+Kamigoro@users.noreply.github.com>
2020-05-21 21:36:48 +0200

    Removing unnecessary tostring function

--- Models/StickyNote.cs
+++ Models/StickyNote.cs
@@ -5,11 +5,5 @@
         public int Id { get; set; }
         public string Text { get; set; }
         public string Color { get; set; }
-
-        public override string ToString()
-        {
-            return $"Text : {Text}";
-        }
-
     }
 }
